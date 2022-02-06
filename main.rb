5f44a4fd954a012b4f9c1370376ee6897070b169
sonia0409 <sonia.singh0409@gmail.com>
2022-02-06 15:11:44 -0500

    to start the game

--- main.rb
+++ main.rb
@@ -1,23 +1,30 @@
-require "./game"
+
 require "./players"
 require "./question"
-require "./answer"
-require "./check"
-require "./winner"
+require "./game"
+require "./turn"
 
 
-# define players
-player1 = Player.new("player1", 3)
-player2 = Player.new("player2", 3)
 
-puts player1.name
-puts player2.name
 
-def gameBegins
-    #player1: question
-    puts "#{player1}"
-    #get answer
-    #check for answer and the sum
-    #display the scores
-    #switch the player
-end
+# Nouns become class and actions becomes method 
+# 1 Game starts!
+# 2 Game Selects the player- let say Player1
+# 3 Game prints question to the Player1 to add the two random numbers
+# 4 Player1 enters the answer.
+# 5 Game gets the answer typed by Player1
+# 6 Game checks if the answer is equal to the 'sum' calculated by the Game.
+# 7 If the sum != answer(by the player) , Game sends out the message to Player1:
+#  and life of the Player reduces by 1
+# 10 If the sum == answer(by the player)
+#  repeat step 8 + life of the player doesnot change.
+# 12 Game prints the scores of both the players
+# 13 Game switch the turn to the Player2
+# 14 Game goes on untill the live == 0 for any of the player
+
+
+
+
+game = Game.new
+
+game.start
