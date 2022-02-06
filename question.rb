b52a324c9da05c47804bd4472e12125eece4be98
sonia0409 <sonia.singh0409@gmail.com>
2022-02-06 15:27:54 -0500

    refractor question method

--- question.rb
+++ question.rb
@@ -1,8 +1,9 @@
 class Question
+    attr_accessor :sum, :display
     def initialize()
         @num1 = rand(1..20)
         @num2 = rand(1..20)
         @sum = @num1 + @num2
-        puts "What does #{@num1} plus #{@num2} equal?"
+        @display ="What does #{@num1} plus #{@num2} equal?"
     end
 end
